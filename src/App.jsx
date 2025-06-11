1f88a01012a96d8a33532db0e26c0fdf936cfc44
SProgrammer13 <Aleksandrafomina2022@gmail.com>
2025-06-11 15:02:55 +0300

    weather infgo added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import { useState } from 'react'
 import './App.css'
 import SearchBar from './assets/components/SearchBar'
 import DisplayedWeather from './assets/components/Displayed';
+import { use } from 'react';
+
+const apiKey = '48926b83330ccdf2c48aaf7a642e4e78';
 
 function App() {
-  function weatherSearch(city){
-    console.log(city);
-  
-}
+  const [weather, setWeather] = useState(null);
+  async function weatherSearch(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&q=${city}&units=metric`
+    const result = await fetch(url);
+    if (result.ok) {
+      const data = await result.json();
+     setWeather(data)
+    }
+    else {
+      console.log("This city doesn't exist!")
+      setWeather(null)
+    }
+
+  }
   return (
     <>
-    <SearchBar weatherSearch = {weatherSearch}></SearchBar>
-    <DisplayedWeather></DisplayedWeather>
+      <SearchBar weatherSearch={weatherSearch}></SearchBar>
+      <DisplayedWeather weather={weather}></DisplayedWeather>
     </>
   )
 }
--- src/assets/components/Displayed.jsx
+++ src/assets/components/Displayed.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
-function DisplayedWeather(weatherSearch){
+function DisplayedWeather({weather}){
+    if(!weather){
+        return <div>Enter the city!</div>
+    }
+
     return (
         <div className="displayed-weather">
-            <h3 className="city-name" weatherSearch = {weatherSearch}></h3>
+            <h3 className="city-name">Погода у місті {weather.name}</h3>
+            <h3>Weather:{weather.weather[0].main}</h3>
+            <h2>Temperature:{weather.main.temp}°C</h2>
+            <h4>Wind:{weather.wind.speed}m/s</h4>
+            {<div>Rain amount: {weather.rain['1h']}</div>} && {weather.rain}
         </div>
     )
 }
