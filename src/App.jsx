466fcafabe8e2f9d2f1c0f6b6f9891998a65cdbc
SProgrammer13 <Aleksandrafomina2022@gmail.com>
2025-06-18 15:17:00 +0300

    forecast added

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,32 @@ const apiKey = '48926b83330ccdf2c48aaf7a642e4e78';
 
 function App() {
   const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   async function weatherSearch(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&q=${city}&units=metric`
     const result = await fetch(url);
     if (result.ok) {
       const data = await result.json();
      setWeather(data)
+     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?&appid=${apiKey}&q=${city}&units=metric`
+    const forecastResult = await fetch(forecastUrl);
+    if (forecastResult.ok) {
+      const data = await forecastResult.json();
+      setForecast(data);
+      console.log(data);
+      }
+      else {
+      alert("This city doesn't exist! Try again.")
+      setWeather(null)
+      setForecast(null)
     }
+  }
+
     else {
       alert("This city doesn't exist! Try again.")
       setWeather(null)
     }
-
   }
   return (
     <>
--- src/assets/components/Displayed.jsx
+++ src/assets/components/Displayed.jsx
@@ -7,14 +7,19 @@ function DisplayedWeather({weather}){
 
     return (
         <div className="displayed-weather">
-            <p className="city-name">Weather in {weather.name}:</p>
+            <p className="city-name">Weather in {weather.name}, {new Intl.DisplayNames('en', {type: 'region'}).of(weather.sys.country)}:</p>
             <div className="forecast">
+                <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" className="weather-icon" />
                 <p className="city-weather">Weather:{weather.weather[0].main}</p>
                 <p className="temp">Temperature:{weather.main.temp}°C</p>
+                <p className="feels-like">Feels like:{weather.main.feels_like}°C</p>
                 <p className="wind">Wind:{weather.wind.speed}m/s</p>
+                <p className="humidity">Humidity:{weather.main.humidity}%</p>
+                <p className="pressure">Pressure:{Math.round(weather.main.pressure * 0.7500615758)}mm</p>
                 {weather.rain && weather.rain['1h'] && (
                     <div>Rain amount: {weather.rain['1h']}</div>
                 )}
+                <p className="sunrise-sunset">Sunrise:{new Date(weather.sys.sunrise * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}, Sunset:{new Date(weather.sys.sunset * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
             </div>
         </div>
     )
