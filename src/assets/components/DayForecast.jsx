483db9861ab2f1e3393458169cf09e6adafa7747
SProgrammer13 <Aleksandrafomina2022@gmail.com>
2025-07-02 15:21:53 +0300

    blocks with weather adeed

--- src/assets/components/DayForecast.jsx
+++ src/assets/components/DayForecast.jsx
@@ -8,8 +8,18 @@ function DayForecast({forecastList}){
             <div className="day-times">
                 {forecastList.map(item => (
                         <div className="time-period">
+                            <img src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="" />
                             <div>
                              {new Date(item.dt * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+                            </div>
+                            <div>
+                                {item.main.temp}°C
+                            </div>
+                            <div>
+                                {item.wind.speed}m/s
+                            </div>
+                            <div>
+                                Humidity:{item.main.humidity}%
                             </div>
                         </div>
                 
