2bc2d0c2c0fb6f6346b3614b740dc33693b8a9a4
SProgrammer13 <Aleksandrafomina2022@gmail.com>
2025-07-16 14:21:28 +0300

    new styles added

--- src/assets/components/DayForecast.jsx
+++ src/assets/components/DayForecast.jsx
@@ -8,10 +8,11 @@ function DayForecast({forecastList}){
             <div className="day-times">
                 {forecastList.map((item, index) => (
                         <div className="time-period" key={index}>
-                            <img className='info icon' src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="" />
-                            <div className="info">
+                             <div className="info time">
                              {new Date(item.dt * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
                             </div>
+                            <img className='info icon' src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="" />
+                             <p className="city-weather">{item.weather[0].main}</p>
                             <div className="info">
                                 {item.main.temp}°C
                             </div>
--- src/assets/components/Displayed.jsx
+++ src/assets/components/Displayed.jsx
@@ -10,8 +10,8 @@ function DisplayedWeather({weather}){
             <p className="city-name">Weather in {weather.name}, {new Intl.DisplayNames('en', {type: 'region'}).of(weather.sys.country)}:</p>
             <div className="forecast">
                 <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" className="weather-icon" />
-                <p className="city-weather">Weather:{weather.weather[0].main}</p>
-                <p className="temp">Temperature:{weather.main.temp}°C</p>
+                <p className="city-weather">{weather.weather[0].main}</p>
+                <p className="temp">{weather.main.temp}°C</p>
                 <p className="feels-like">Feels like:{weather.main.feels_like}°C</p>
                 <p className="wind">Wind:{weather.wind.speed}m/s</p>
                 <p className="humidity">Humidity:{weather.main.humidity}%</p>
